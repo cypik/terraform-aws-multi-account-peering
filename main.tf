3f346246eb01ef0a0b9f628bdc1ca0b7b531d362
sohanyadav <sohanyadavdevops@gmail.com>
2025-04-17 15:41:29 +0530

    Initial commit

---
+++ main.tf
@@ -0,0 +1,149 @@
+module "labels" {
+  source  = "cypik/labels/aws"
+  version = "1.0.2"
+
+  name        = var.name
+  environment = var.environment
+  attributes  = var.attributes
+  repository  = var.repository
+  managedby   = var.managedby
+  label_order = var.label_order
+}
+
+
+provider "aws" {
+  alias   = "accepter"
+  region  = var.accepter_region
+  profile = var.profile_name
+
+  assume_role {
+    role_arn = var.accepter_role_arn
+  }
+}
+
+data "aws_caller_identity" "peer" {
+  provider = aws.accepter
+}
+
+data "aws_region" "peer" {
+  provider = aws.accepter
+}
+
+
+resource "aws_vpc_peering_connection" "default" {
+  count         = var.enable_peering == true ? 1 : 0
+  peer_owner_id = data.aws_caller_identity.peer.account_id
+  peer_region   = data.aws_region.peer.id
+  vpc_id        = var.requestor_vpc_id
+  peer_vpc_id   = var.acceptor_vpc_id
+  auto_accept   = false
+  tags = merge(
+    module.labels.tags,
+    {
+      "Name" = format("%s-%s", module.labels.name, module.labels.environment)
+    }
+  )
+}
+
+resource "aws_vpc_peering_connection_accepter" "peer" {
+  count                     = var.enable_peering == true ? 1 : 0
+  provider                  = aws.accepter
+  vpc_peering_connection_id = aws_vpc_peering_connection.default[0].id
+  auto_accept               = true
+  tags                      = module.labels.tags
+}
+
+
+data "aws_vpc" "requestor" {
+  count = var.enable_peering == true ? 1 : 0
+  id    = var.requestor_vpc_id
+}
+
+data "aws_route_table" "requestor" {
+  count = var.enable_peering == true ? length(distinct(sort(data.aws_subnets.requestor[0].ids))) : 0
+
+  subnet_id = element(
+    distinct(sort(data.aws_subnets.requestor[0].ids)),
+    count.index
+  )
+}
+
+data "aws_subnets" "requestor" {
+  count = var.enable_peering == true ? 1 : 0
+
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.requestor[0].id]
+  }
+}
+
+
+data "aws_vpc" "acceptor" {
+  provider = aws.accepter
+  count    = var.enable_peering == true ? 1 : 0
+  id       = var.acceptor_vpc_id
+}
+
+
+data "aws_subnets" "acceptor" {
+  provider = aws.accepter
+  count    = var.enable_peering == true ? 1 : 0
+
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.acceptor[0].id]
+  }
+}
+
+
+data "aws_route_tables" "acceptor" {
+  provider = aws.accepter
+  count    = var.enable_peering == true ? length(distinct(sort(data.aws_subnets.acceptor[0].ids))) : 0
+
+  vpc_id = data.aws_vpc.acceptor[0].id
+}
+
+
+resource "aws_route" "requestor" {
+
+  count = var.enable_peering == true ? length(
+    distinct(sort(data.aws_route_table.requestor.*.route_table_id))
+  ) * length(data.aws_vpc.acceptor[0].cidr_block_associations) : 0
+
+  route_table_id = element(
+    distinct(sort(data.aws_route_table.requestor.*.route_table_id)),
+    ceil(
+      count.index / length(data.aws_vpc.acceptor[0].cidr_block_associations)
+    )
+  )
+
+  destination_cidr_block    = data.aws_vpc.acceptor.0.cidr_block_associations[count.index % length(data.aws_vpc.acceptor[0].cidr_block_associations)]["cidr_block"]
+  vpc_peering_connection_id = aws_vpc_peering_connection.default[0].id
+  depends_on = [
+    data.aws_route_table.requestor,
+    aws_vpc_peering_connection.default,
+  ]
+}
+
+
+resource "aws_route" "acceptor" {
+  provider = aws.accepter
+
+  count = var.enable_peering == true ? length(
+    distinct(sort(data.aws_route_tables.acceptor[0].ids))
+  ) * length(data.aws_vpc.requestor[0].cidr_block_associations) : 0
+
+  route_table_id = element(
+    distinct(sort(data.aws_route_tables.acceptor[0].ids)),
+    ceil(
+      count.index / length(data.aws_vpc.requestor[0].cidr_block_associations)
+    )
+  )
+
+  destination_cidr_block    = data.aws_vpc.requestor.0.cidr_block_associations[count.index % length(data.aws_vpc.requestor[0].cidr_block_associations)]["cidr_block"]
+  vpc_peering_connection_id = aws_vpc_peering_connection.default[0].id
+  depends_on = [
+    data.aws_route_tables.acceptor,
+    aws_vpc_peering_connection.default,
+  ]
+}
---
+++ variables.tf
@@ -0,0 +1,87 @@
+variable "name" {
+  type        = string
+  default     = ""
+  description = "Name  (e.g. `app` or `cluster`)."
+}
+
+
+variable "environment" {
+  type        = string
+  default     = ""
+  description = "Environment (e.g. `prod`, `dev`, `staging`)."
+}
+
+variable "label_order" {
+  type        = list(any)
+  default     = ["name", "environment"]
+  description = "Label order, e.g. `name`,`application`."
+}
+
+variable "tags" {
+  type        = map(string)
+  default     = {}
+  description = "Additional tags (e.g. map(`BusinessUnit`,`XYZ`)."
+}
+
+
+variable "accepter_region" {
+  type        = string
+  description = "The region of acceptor vpc."
+}
+
+variable "enable_peering" {
+  type        = bool
+  default     = false
+  description = "Set to false to prevent the module from creating or accessing any resources."
+}
+
+variable "requestor_vpc_id" {
+  type        = string
+  description = "Requestor VPC ID."
+}
+
+variable "acceptor_vpc_id" {
+  type        = string
+  description = "Acceptor VPC ID."
+}
+
+variable "acceptor_allow_remote_vpc_dns_resolution" {
+  type        = bool
+  default     = true
+  description = "Allow acceptor VPC to resolve public DNS hostnames to private IP addresses when queried from instances in the requestor VPC."
+}
+
+variable "requestor_allow_remote_vpc_dns_resolution" {
+  type        = bool
+  default     = true
+  description = "Allow requestor VPC to resolve public DNS hostnames to private IP addresses when queried from instances in the acceptor VPC."
+}
+
+variable "accepter_role_arn" {
+  type        = string
+  default     = ""
+  description = "The Role ARN of accepter AWS account."
+}
+variable "profile_name" {
+  type        = string
+  default     = null
+  description = "Name of aws profile."
+}
+
+variable "attributes" {
+  type        = list(any)
+  default     = []
+  description = "Additional attributes (e.g. `1`)."
+}
+
+variable "managedby" {
+  type        = string
+  default     = "info@cypik.com"
+  description = "ManagedBy, eg 'info@cypik.com'"
+}
+
+variable "repository" {
+  type        = string
+  default     = "https://github.com/cypik/terraform-aws-multi-account-peering"
+  description = "Terraform current module repo"
+}
