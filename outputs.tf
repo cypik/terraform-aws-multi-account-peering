853479618470a5d7cb09b80762f5c7db759a1f39
cypiksuresh <suresh.y@cypik.com>
2025-04-23 14:41:22 +0530

    update modules

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,10 @@
 output "connection_id" {
-  value       = join("", aws_vpc_peering_connection.default.*.id)
+  value       = join("", [for conn in aws_vpc_peering_connection.default : conn.id])
   description = "VPC peering connection ID."
 }
 
 output "accept_status" {
-  value       = join("", aws_vpc_peering_connection.default.*.accept_status)
+  value       = join("", [for conn in aws_vpc_peering_connection.default : conn.accept_status])
   description = "The status of the VPC peering connection request."
 }
 
